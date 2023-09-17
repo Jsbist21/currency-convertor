913add0884b6ba9f02ffde13827a05f8c97a01e5
Jsbist21 <bishtjatin477@gmail.com>
2023-09-17 18:59:36 +0530

    Currency-convertor

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+const useCurrencyInfo = (currency) => {
+
+    const [data, setData] = useState({});
+ 
+    useEffect(() => {
+        axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => setData(res[currency]))
+
+    },[currency])
+
+    return data;
+
+
+  
+}
+
+export default useCurrencyInfo;
